276d5223bf4fd2747f3d0ac881a4dfd1187c575c
haresh <hdpatel234@gmail.com>
2026-09-11 23:49:44 +0530

    feat: add Layout component and global Tailwind CSS setup

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -6,9 +6,9 @@ import { ScrollToTop } from "./ScrollToTop";
 
 export function Layout({ children }: { children: React.ReactNode }) {
     return (
-        <div className="relative flex min-h-screen flex-col">
+        <div className="relative flex min-h-screen flex-col w-full max-w-full overflow-x-hidden">
             <Header />
-            <main className="flex-1">{children}</main>
+            <main className="flex-1 w-full max-w-full overflow-x-hidden">{children}</main>
             <Footer />
             <ScrollToTop />
         </div>
